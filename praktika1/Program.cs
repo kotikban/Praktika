9d6d2922143594f2eaab68b194a8798eac6b1783
student <student@C251U51>
2020-09-11 17:47:33 +0300

    a

---
+++ praktika1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace praktika1
+{
+  class Program
+  {
+    struct Person
+    {
+      public string firstName;
+      public string lastName;
+      public int age;
+      public Person(string _firstName, string _lastName, int _age)
+      {
+        firstName = _firstName;
+        lastName = _lastName; age = _age;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + lastName + ", age " + age;
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      Person p = new Person("Van", "Darkholme", 47);
+      Console.WriteLine(p);
+    }
+  }
+}
