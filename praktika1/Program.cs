e5338994669f78c61d03e51c2b8103a17c22cdaf
student <student@C251U51>
2020-09-11 18:48:16 +0300

    Вторая часть

--- praktika1/Program.cs
+++ praktika1/Program.cs
@@ -9,21 +9,25 @@ namespace praktika1
       public string firstName;
       public string lastName;
       public int age;
-      public Person(string _firstName, string _lastName, int _age)
+      public enum Genders : int { Male, Female };
+      public Genders gender;
+      public Person(string _firstName, string _lastName, int _age, Genders _gender)
       {
         firstName = _firstName;
-        lastName = _lastName; age = _age;
+        lastName = _lastName; 
+        age = _age;
+        gender = _gender;
       }
       public override string ToString()
       {
-        return firstName + " " + lastName + ", age " + age;
+        return firstName + " " + lastName + "(" + gender + "), age " + age;
       }
     }
 
     static void Main(string[] args)
     {
-      Person p = new Person("Van", "Darkholme", 47);
-      Console.WriteLine(p);
+      Person p = new Person("Van", "Darkholme", 47, Person.Genders.Male);
+      Console.WriteLine(p.ToString());
     }
   }
 }
